80a7b5c6670399a432862da20aba16d1ac407920
Jannik Wibker <jannikwibker@gmail.com>
2022-10-15 12:23:19 +0200

    add basic header component

---
+++ components/Header.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import ChevronLeft from '../icons/ChevronLeft'
+import ChevronRight from '../icons/ChevronRight'
+
+type HeaderProps = {
+  date: Date,
+  onPrev: () => void,
+  onNext: () => void,
+  canPrev: boolean,
+  canNext: boolean
+}
+
+const Header = ({ date, onPrev, onNext, canPrev, canNext }: HeaderProps) => {
+  return (
+    <div className="">
+      <h1 className="p-4 text-4xl font-semibold">Fahrplan</h1>
+
+      <div className="flex">
+        <div className={'p-4 rounded-full ' + (canPrev ? 'text-black' : 'text-slate-300')} onClick={canPrev ? onPrev : undefined}><ChevronLeft style={{ width: 36, height: 36 }} /></div>
+        <div className="grow text-2xl font-semibold text-center leading-[calc(36px+2rem)]">
+          {date.toLocaleDateString('de-DE', { weekday: 'long' })} - {date.toLocaleDateString('de-DE', { day: 'numeric', month: 'numeric' })}
+        </div>
+        <div className={'p-4 rounded-full ' + (canPrev ? 'text-black' : 'text-slate-300')} onClick={canNext ? onNext : undefined}><ChevronRight style={{ width: 36, height: 36 }} /></div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
