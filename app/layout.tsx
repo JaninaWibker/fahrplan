0035b39051a263f6f1e8b3540383c2c37b0c4869
Jannik Wibker <jannikwibker@gmail.com>
2023-09-17 18:33:23 +0200

    Update readme (#2)

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,10 +7,9 @@ const inter = Inter({
 })
 
 export const metadata = {
-  title: 'Fahrplan | kitmatheinfo.de',
-  description: 'O-Phasen Termine: Das Wann und Wo',
+  title: 'Fahrplan',
   icons: {
-    icon: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🦆</text></svg>'
+    icon: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🗓️</text></svg>'
   }
 }
 
