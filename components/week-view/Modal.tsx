7c370a5058e4ffa942f4f6f9bed8b8fcba895191
Jannik Wibker <jannikwibker@gmail.com>
2023-12-08 18:40:44 +0100

    display differently styled modals based on event priority

--- components/week-view/Modal.tsx
+++ components/week-view/Modal.tsx
@@ -2,21 +2,57 @@
 
 import type { PropsWithChildren, ReactNode } from 'react'
 import * as Popover from '@radix-ui/react-popover'
+import { BadgeCheckIcon, XIcon } from 'lucide-react'
+import { clsx } from 'clsx'
+import type { AccentColorBasedStyle, AccentColorVariant } from '@/utils/constants'
+
+const MODAL_STYLE_PER_ACCENT_COLOR_VARIANT = {
+  blue: 'outline-blue-200',
+  pink: 'outline-pink-300',
+  green: 'outline-green-300',
+  purple: 'outline-purple-300',
+  orange: 'outline-orange-300',
+  sky: 'outline-sky-300',
+  fuchsia: 'outline-fuchsia-300'
+} as const satisfies AccentColorBasedStyle
+
+const CLOSE_BTN_STYLE_PER_ACCENT_COLOR_VARIANT = {
+  blue: 'text-blue-600 hover:bg-blue-100',
+  pink: 'text-pink-600 hover:bg-pink-100',
+  green: 'text-green-600 hover:bg-green-100',
+  purple: 'text-purple-600 hover:bg-purple-100',
+  orange: 'text-orange-600 hover:bg-orange-100',
+  sky: 'text-sky-600 hover:bg-sky-100',
+  fuchsia: 'text-fuchsia-600 hover:bg-fuchsia-100'
+} as const satisfies AccentColorBasedStyle
 
 type ModalProps = PropsWithChildren<{
   title: string
+  verified?: boolean
+  accentColorVariant?: AccentColorVariant
   trigger: ReactNode
   open: boolean
   onOpenChange: (open: boolean) => void
 }>
 
-export const Modal = ({ title, trigger, children, open, onOpenChange }: ModalProps) => {
+export const Modal = ({
+  title,
+  verified = false,
+  accentColorVariant = 'pink',
+  trigger,
+  children,
+  open,
+  onOpenChange
+}: ModalProps) => {
   return (
     <Popover.Root open={open} onOpenChange={onOpenChange}>
       <Popover.Trigger asChild>{trigger}</Popover.Trigger>
       <Popover.Portal>
         <Popover.Content
-          className="z-20 mb-3 flex max-h-[400px] w-[352px] flex-col overflow-y-scroll rounded-lg bg-white shadow-lg outline outline-[3px] outline-pink-300"
+          className={clsx(
+            'z-20 mb-3 flex max-h-[400px] w-[352px] flex-col overflow-y-scroll rounded-lg bg-white shadow-lg outline outline-[3px]',
+            MODAL_STYLE_PER_ACCENT_COLOR_VARIANT[accentColorVariant]
+          )}
           side="left"
           align="start"
           sideOffset={10}
@@ -24,7 +60,21 @@ export const Modal = ({ title, trigger, children, open, onOpenChange }: ModalPro
         >
           <div className="flex-1 rounded-xl bg-white p-4">
             <div className="mx-auto max-w-md">
-              <h2 className="mb-4 font-medium">{title}</h2>
+              <div className="flex items-start justify-between">
+                <div className="flex items-start">
+                  <h2 className="mb-4 font-medium">{title}</h2>
+                  {verified && <BadgeCheckIcon className="ml-1 mt-0.5 h-5 w-5 shrink-0 stroke-[2.5px]" />}
+                </div>
+                <Popover.Close
+                  className={clsx(
+                    '-mr-1 -mt-1 rounded-full p-1',
+                    CLOSE_BTN_STYLE_PER_ACCENT_COLOR_VARIANT[accentColorVariant]
+                  )}
+                  aria-label="Close"
+                >
+                  <XIcon className="h-6 w-6" />
+                </Popover.Close>
+              </div>
               {children}
             </div>
           </div>
--- utils/constants.ts
+++ utils/constants.ts
@@ -1,12 +1,20 @@
 export const HEIGHT_PER_HOUR = 56
 export const MARGIN_EVENTS = 2
 
-export const COLORS_PER_PRIORITY: Record<number, string> = {
-  1: 'bg-blue-100 text-blue-700 hover:bg-blue-200',
-  2: 'bg-pink-100 text-pink-700 hover:bg-pink-200',
-  3: 'bg-green-100 text-green-700 hover:bg-green-200',
-  4: 'bg-purple-100 text-purple-700 hover:bg-purple-200',
-  5: 'bg-orange-100 text-orange-700 hover:bg-orange-200',
-  6: 'bg-sky-100 text-sky-700 hover:bg-sky-200',
-  7: 'bg-fuchsia-100 text-fuchsia-700 hover:bg-fuchsia-200'
+// can be used to map a proprity to a color as well (using `priority - 1` as index)
+// reorder this to change the assignment of colors to priorities
+export const ACCENT_COLOR_VARIANTS = ['blue', 'pink', 'green', 'purple', 'orange', 'sky', 'fuchsia'] as const
+
+export type AccentColorVariant = (typeof ACCENT_COLOR_VARIANTS)[number]
+
+export type AccentColorBasedStyle = Record<AccentColorVariant, string>
+
+export const COLORS_PER_PRIORITY: AccentColorBasedStyle = {
+  blue: 'bg-blue-100 text-blue-700 hover:bg-blue-200',
+  pink: 'bg-pink-100 text-pink-700 hover:bg-pink-200',
+  green: 'bg-green-100 text-green-700 hover:bg-green-200',
+  purple: 'bg-purple-100 text-purple-700 hover:bg-purple-200',
+  orange: 'bg-orange-100 text-orange-700 hover:bg-orange-200',
+  sky: 'bg-sky-100 text-sky-700 hover:bg-sky-200',
+  fuchsia: 'bg-fuchsia-100 text-fuchsia-700 hover:bg-fuchsia-200'
 }
